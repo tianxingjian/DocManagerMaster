f5b55185fe12968dcb78d3220176eeaafb3fa3bb
tianxingjian <w520zheng@gmail.com>
2014-12-22 11:31:09 +0800

    脚本修改

--- sqlscript/docmanager.sql
+++ sqlscript/docmanager.sql
@@ -9,7 +9,7 @@ CREATE TABLE
         `projectName` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '项目信息',
         `versionNo` VARCHAR(30) DEFAULT '~' COMMENT '版本号',
         `relpath` VARCHAR(20) NULL COMMENT '附件存储相对路径',
-        `ts` DATETIME DEFAULT NOW() COMMENT '记录增加时间',
+        `ts` DATETIME COMMENT '记录增加时间',
         PRIMARY KEY (`bugId`)
     )
     ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
@@ -21,7 +21,7 @@ CREATE TABLE
         `id` bigint(5) NOT NULL AUTO_INCREMENT COMMENT 'id',
         `bugId` char(13) NOT NULL COMMENT '问题信息Id',
         `filename` VARCHAR(20) DEFAULT '~' COMMENT '文件名称',
-        `ts` DATETIME DEFAULT NOW() COMMENT '记录增加时间',
+        `ts` DATETIME COMMENT '记录增加时间',
         PRIMARY KEY (`id`),
         FOREIGN KEY (bugId) REFERENCES doc_bugmessage(bugId)
                      ON DELETE CASCADE
